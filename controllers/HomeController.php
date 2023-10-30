60ebd108ea501e6e0e3c3ed6bdbd894ae5576dce
Neij <ju.legoff29@gmail.com>
2023-10-30 16:45:41 +0100

    CleanUp

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -6,7 +6,6 @@ class HomeController extends AppController {
     public function __construct() {
         parent::__construct("home", "views/home.phtml");
     }
-
 }
 
 
--- controllers/WebController.php
+++ controllers/WebController.php
@@ -2,7 +2,6 @@
 
 namespace Controllers;
 
-
 class WebController {
     public $template;
 

e3228cd39db8e63f88f0d3eff5b24ddc51fb0fb5
Benjamin PLOUZENNEC <plouzennec.benjamin@gmail.com>
2023-10-30 16:38:33 +0100

    Removing old FastCV folder

--- FastCV/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('DB_HOST')) {
-    define("DB_HOST", 'localhost');
-    define("DB_NAME", 'julienlegoff_fastcv');
-    define("DB_USER", 'root');
-    define("DB_PASS", '');
-}

61593a4b6730e3804619ff1ba2bdd58c410eb2b3
Benjamin PLOUZENNEC <plouzennec.benjamin@gmail.com>
2023-10-30 16:38:33 +0100

    Removing old FastCV folder

--- FastCV/controllers/PolitiqueController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Controllers;
-
-class PolitiqueController extends WebController {
-        public function __construct() {
-            parent::__construct("views/politique.phtml"); 
-        }
-}
--- FastCV/migrations/02_22-10-2023.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users 
-ADD login_attempts INT NOT NULL DEFAULT(0);
-
-ALTER TABLE users
-ADD last_failed_login DATE;
